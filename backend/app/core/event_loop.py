54f263784c51264ce08e6a556a3ba42195cf0cd2
Yadian Zhao <yadian.zhao@pg.canterbury.ac.nz>
2025-03-21 15:03:06 +1300

    first upload

--- backend/app/core/event_loop.py
+++
@@ -1,5 +0,0 @@
-import asyncio
-
-# 全局事件循环变量
-main_event_loop: asyncio.AbstractEventLoop = asyncio.new_event_loop()
-
--- backend/app/websocket/manager.py
+++
@@ -1,6 +0,0 @@
-# app/websocket/manager.py
-from collections import defaultdict
-
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections = defaultdict(dict)
